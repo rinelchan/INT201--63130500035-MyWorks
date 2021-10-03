98e5c8634265d93f1b74fbc281eca818fdd48a20
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2021-10-04 01:23:36 +0700

    add commit

---
+++ JavaScriptBasic/var let const(variable).js
@@ -0,0 +1,24 @@
+
+let human = 'พี่กั่ย';
+var human = 'พี่กั่ย';
+const human = 'พี่กั่ย';
+้ีhuman = 'พี่นก'
+
+if (2 > 1) {
+    let fish = 'ปลาหยุด';
+    fish = 'ปลาวิด';
+    console.log(human);  // 1
+    console.log(fish);   // 2
+}
+
+console.log(human);      // 3
+console.log(fish);       // 4
+  
+/* 
+scope = ขอบเขตของโค้ด - น้ำทะเล
+global scope = พื้นดิน
+
+let = ตัวแปร                     --> ปกติ
+const(constant) = ค่าคงที่         --> เปลี่ยนไม่ได้
+var = ตัวแปรที่เปลี่ยนค่าได้ - เกเร     --> เอาหมดเลย ไม่สนไรทั้งนั้น
+*/
