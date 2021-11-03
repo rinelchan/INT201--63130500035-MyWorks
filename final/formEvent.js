de23639e8fc58704e4f90979600cb15ba4ab40a7
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2021-11-03 15:08:05 +0700

    add

--- final/formEvent.js
+++ final/formEvent.js
@@ -5,14 +5,14 @@ let answerForm = document.querySelector('.answer-form');
 function checkAnswer(event) {
     let tooLong = false;
     answers.forEach((answer) => {
-        if (answer.ariaValueMax.length > 20) {
+        if (answer.valueMax.length > 20) {
             tooLong = true;
         }
     });
     if (tooLong) {
-        errorMessage.getElementsByClassName.display = 'block';
+        errorMessage.style .display = 'block';
         event.preventDefault();
     }
 }
 
-answerForm.addEventListener('submit', checkAnswer);
+answerForm.addEventListener('input', 'submit', checkAnswer);
