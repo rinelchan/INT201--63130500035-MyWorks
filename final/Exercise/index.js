096b9972dc9d71fd675d3580f10439151b3c77ef
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2021-11-20 18:08:05 +0700

    add

--- final/Exercise/index.js
+++ final/Exercise/index.js
@@ -1,10 +1,17 @@
+// อวตารร่างของแต่ละส่วน
 let numberInput = getElementById('number-input');
 let runButton = getElementById('run-button');
 let output = getElementById('output');
 
+// ใส่ลูกเล่นเพิ่ม
 function sutkun() {
     let number = Number(numberInput.value);
     let outputHtml = '';
+
+    if (number === 0) {
+        output.innerHTML = 'งุงุิ'ว
+        return;
+    }
 
     for (let i = 0; i <= 12; i++) {
         outputHtml += '<p>';
@@ -12,7 +19,8 @@ function sutkun() {
         outputHtml += '</p>';
     }
 
-    output.innerHtml = outputHtml;
+    output.innerHTML = outputHtml;
 }
 
+// ให้ปุ่มมีฟังก์ชันการทำงานจากการอ้างอิง
 output.addEventListerner('click',sutkun);
