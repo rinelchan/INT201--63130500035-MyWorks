90bf1fb71e77644a9ec6ffac31208b76f86412f9
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2021-11-22 23:31:51 +0700

    add

--- final/Review/Functional Programming/2 Higher-Order Functions.js
+++ final/Review/Functional Programming/2 Higher-Order Functions.js
@@ -6,6 +6,21 @@
 //// 4.1 Returning a function
 //// 4.2 Accepting a function as an argument
 
-// filter 
-// map
-// reduce
+const persons = [
+    { name: "John", age:17 },
+    { name: "Jane", age:10 },
+    { name: "Jim", age:15 }
+];
+
+//// filter 
+// สร้าง Array ใหม่
+const kids = [];
+for (let i = 0; i < persons.length; i++) {
+    const person = persons[i];
+    if (person.age <= 15) {
+        kids.push(person);
+    }
+}
+console.log('Kids: ', kids);
+//// map
+//// reduce
