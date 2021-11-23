a2fe4791f3187bde70c2b2a0968cd62450c0a4bd
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2021-11-23 22:14:48 +0700

    add

--- final/Review/Functional Programming/2 Higher-Order Functions.js
+++ final/Review/Functional Programming/2 Higher-Order Functions.js
@@ -12,15 +12,72 @@ const persons = [
     { name: "Jim", age:15 }
 ];
 
+
 //// filter 
-// สร้าง Array ใหม่
-const kids = [];
+// แบบปกติ
+const kids = [];                            // สร้าง Array ใหม่
 for (let i = 0; i < persons.length; i++) {
     const person = persons[i];
     if (person.age <= 15) {
         kids.push(person);
     }
 }
+
+// แบบย่อ (ใช้ filter)
+const kids = persons.filter(person => {
+    return person.age <=15;
+});
+
+// แบบย่อค่ดพ่อค่ดแม่!! (ใช้ filter)
+const kids = persons.filter(person => person.age <=15);
+
 console.log('Kids: ', kids);
+
+
+
 //// map
+// แบบปกติ
+const olderPersons = [];
+for (let i = 0; i < persons.length; i++) {
+    const person = persons[i];
+    olderPersons.push({
+        ...person,
+        age: person.age * 2
+    });
+}
+
+// แบบย่อ (ใช้ map)
+const olderPersons = persons.map((person) => {
+    return {
+        ...person,
+        age: person.age * 2
+    }
+});
+
+// แบบย่อค่ดพ่อค่ดแม่!! (ใช้ map)
+const olderPersons = persons.map((person) => ({
+        ...person,
+        age: person.age * 2
+}));
+
+console.log('Older persons: ' , olderPersons);
+
+
+
 //// reduce
+// แบบปกติ
+let totalAge = 0;
+for (let i = 0; i < persons.length; i++) {
+    const person = persons[i];
+    totalAge += person.age; 
+}
+
+// แบบย่อ (ใช้ reduce)
+const totalAge = persons.reduce((age, person) => {
+    return age + person.age;
+},0);
+
+// แบบย่อค่ดพ่อค่ดแม่!! (ใช้ reduce)
+const totalAge = persons.reduce((age, person) => age + person.age, 0);
+
+console.log('Total age: ', totalAge);
