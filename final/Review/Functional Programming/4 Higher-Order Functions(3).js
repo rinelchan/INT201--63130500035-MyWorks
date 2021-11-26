ca0f304ebc0d005cd9a4cbb34f63e068c1c3f447
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2021-11-26 23:48:11 +0700

    add

--- final/Review/Functional Programming/4 Higher-Order Functions(3).js
+++ final/Review/Functional Programming/4 Higher-Order Functions(3).js
@@ -6,5 +6,21 @@ const persons = [
 
 
 
-//myFilter
-//myMap
+//// myFilter
+ 
+// filter
+// สร้าง array เปล่าแล้ว วน loop persons แต่ละคน แล้ว assign person แต่ละคนไปให้ argument ของ function ที่สร้างขึ้นมา
+// แล้วเรียกใช้ function และดูเงื่อนไขว่าเป็นจริงหรือไม่
+// ถ้าจริง จะ push ใส่เข้าไปใน array
+// เมื่อวนครบทุก index ใน array หลังจากนั้น filter จะ return array ออกมา
+const kids = persons.filter(person => person.age <= 15);
+console.log('Kids: ' , kids);
+
+// myFilter
+// รับค่า argument เป็น array, function
+
+
+
+
+
+// myMap
