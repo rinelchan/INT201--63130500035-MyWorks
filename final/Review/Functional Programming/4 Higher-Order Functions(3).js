2e85b55da38804e9873a333fa00a303830054232
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2021-11-27 00:14:48 +0700

    add

--- final/Review/Functional Programming/4 Higher-Order Functions(3).js
+++ final/Review/Functional Programming/4 Higher-Order Functions(3).js
@@ -13,14 +13,81 @@ const persons = [
 // แล้วเรียกใช้ function และดูเงื่อนไขว่าเป็นจริงหรือไม่
 // ถ้าจริง จะ push ใส่เข้าไปใน array
 // เมื่อวนครบทุก index ใน array หลังจากนั้น filter จะ return array ออกมา
+
 const kids = persons.filter(person => person.age <= 15);
 console.log('Kids: ' , kids);
 
 // myFilter
 // รับค่า argument เป็น array, function
+function myFilter(arr, callback) {
+    const result = [];
+    for (let i =0; i < arr.length; i++) {
+        const element = arr[i];
+        if (callback(element)) {
+            result.push(element);
+        }
+    }
+    return result;
+}
+const kids = myFilter(persons, person => person.age <= 15);
+console.log('Kids: ' , kids);
+
+// ถ้าผูกติดกับ class array (ปกติไม่ควรทำ!! ไม่นิยม สร้าง class ใหม่เลยดีก่า)
+Array.prototype.myFilter = function myFilter(callback) {
+    const result = [];
+    for (let i =0; i < this.length; i++) {
+        const element = this[i];
+        if (callback(element)) {
+            result.push(element);
+        }
+    }
+    return result;
+}
+const kids = persons.myFilter(person => person.age <= 15);
+console.log('Kids: ' , kids);
 
 
 
+//// myMap
 
+// map
+const olderPersons = persons.map((person) => ({
+    ...person,
+    age: person.age * 2
+}));
+
+console.log('Older persons: ' , olderPersons);
 
 // myMap
+function myMap(arr, callback) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        const element  = arr[i];
+        result.push(callback(element));
+    }
+    return result;
+}
+
+const olderPersons = myMap(persons, (person) => ({
+    ...person,
+    age: person.age * 2
+}));
+
+console.log('Older persons: ' , olderPersons);
+
+// ถ้าผูกติดกับ class array
+Array.prototype.myMap = function myMap(callback) {
+    const result = [];
+    for (let i = 0; i < this.length; i++) {
+        const element  = this[i];
+        result.push(callback(element));
+    }
+    return result;
+}
+
+const olderPersons = persons.myMap((person) => ({
+    ...person,
+    age: person.age * 2
+}));
+
+console.log('Older persons: ' , olderPersons);
---
+++ final/Review/Functional Programming/5 Closures.js
@@ -0,0 +1,3 @@
+// What's Closures ?
+// Data privacy
+// Stateful function
