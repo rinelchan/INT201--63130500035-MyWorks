eee4fabc46bd48b5e2b76bc6cffdd93d8a894a78
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2021-11-28 22:34:31 +0700

    add

---
+++ final/Review/Functional Programming/6 Recursion.js
@@ -0,0 +1,9 @@
+// What' s Recursion ?
+
+// Why/when should we use Recursion ? | ทำไม/เมื่อไหร่ เราควรใช้ Recursion
+////  1. Shorter and more readable                  | 1. เขียนโค้ดได้สั้นลง และทำให้โค้ดอ่านได้ง่ายขึ้น
+////  2. A problem can be built off of sub-problems | 2. ใช้เมื่อปัญหาสามารถแตกออกเป็นปัญหาย่อยๆ ได้
+  
+// How to use Recursion ? | วิธีการสร้าง Recursion
+//// 1. Base case
+//// 2. Recursive case
