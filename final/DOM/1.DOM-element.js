04a0f14b1962c3fe25c4c6f137226a42343bb81a
Nutwara Boonyangkongkaew <nutwara.nice@mail.kmutt.ac.th>
2021-12-08 02:15:27 +0700

    add

---
+++ final/DOM/1.DOM-element.js
@@ -0,0 +1,15 @@
+let message = document.getElementById('message-2');
+message.innerHTML = 'อยากนอน';
+
+let messages = document.getElementsByClassName('message');
+for (let i = 0; i < message.length; i++) {
+    message[i].innerHTML = "พี่ชอบหนูตรงไหนให้เอาปากกามาวงงง' !!"
+}
+
+let messages = document.querySelectorAll('.message');
+messages.forEach((message) => {
+    message.innerHTML = "หมาเลย";
+});
+
+let message = document.querySelector('.message');
+messages.innerHTML = "ไปนอน";
